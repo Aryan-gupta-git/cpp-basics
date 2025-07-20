810e88eb4e53dc8cc729d0ad71db7e09d0efb0f4
Aryan-gupta-git <its.aryangupta020@gmail.com>
2025-07-20 16:34:25 +0530

    Variables

---
+++ variables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int age= 21;
+    float height = 5.9;
+    char favoriteLetter = 'K';
+    bool lovesCoding = true;
+    string name = "Aryan";
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << " feet" << endl;    
+    cout << "Favorite Letter: " << favoriteLetter << endl;
+    cout << "Loves Coding: " << lovesCoding << endl;
+    return 0;
+}
